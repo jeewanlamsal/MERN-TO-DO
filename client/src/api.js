24c9f90a31416049de67cb4c91124b93c4975f8b
jeewanlamsal <106655205+jeewanlamsal@users.noreply.github.com>
2025-12-01 19:43:24 +0545

    Change API base URL to production server new

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://mern-to-do-p7zm.onrender.com',
 });
 
 // Automatically attach token
